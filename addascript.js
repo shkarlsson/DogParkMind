286cbc0e7947827fe62e41283c5915bd622be256
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-05-31 10:50:36 +0200

    fix go to current loc

--- addascript.js
+++ addascript.js
@@ -21,37 +21,52 @@ var map = L.map('map', {
 
 var parkMarkerToBeAdded, popupContent; // replace marker
 
-map.on('click', function(e) {
-	var popupContent = ''
-	popupContent += '<form style="width:200px" js_action="https://script.google.com/macros/s/AKfycbyHBSts9-u_ixD-ZfidpQpABO_173B9OaklBGCT/exec" id="gform">'
-	popupContent += '<input type="text" name="ParkName" class="form-control" value="" placeholder="New park name" pattern=".{2,}" required title="2 characters minimum">'
-	popupContent += '<input type="text" name="LocationDescription" class="form-control" value="" placeholder="Location Description" pattern=".{10,}" required title="10 characters minimum">'
-	popupContent += '<input type="hidden" value="aNewPark" name="WhichPark">'
-	popupContent += '<input type="hidden" value="' + e.latlng.lng + '" name="ParkLocationLon">'
-	popupContent += '<input type="hidden" value="' + e.latlng.lat + '" name="ParkLocationLat">'
-	popupContent += '<button type="button" id="submit_button" class="btn btn-primary" onClick="jsSubmitForm(this.form)">Submit</button>'
-	popupContent += '</form>'
-	if (typeof(parkMarkerToBeAdded) === 'undefined') {
-		parkMarkerToBeAdded = new L.circle(e.latlng, {
-			draggable: true,
-			radius: 0,
-			weight: 0
-		});
-		parkMarkerToBeAdded.bindPopup(popupContent, {
-			maxWidth: "auto"
-		})
-		parkMarkerToBeAdded.addTo(map);
-		/*$("#gform").submit(function() {
-			jsSubmitForm($("#gform"))
-			return false;
-		});*/
-	} else {
-		parkMarkerToBeAdded.setLatLng(e.latlng);
+map.on({
+	click: function(e) {
+		if (e.originalEvent.target.id == 'map') { //Grejen efter && gör så att detta endast händer när man klickar på baskartan, inte på en feature.
+			//console.log(e.originalEvent.target.id)
+			clearActiveSelectedParking()
+		}
 	}
-	parkMarkerToBeAdded.bindPopup(popupContent)
+})
 
-	parkMarkerToBeAdded.openPopup()
-});
+map.on({
+	click: function(e) {
+		if (e.originalEvent.target.id == 'map') { //Grejen efter && gör så att detta endast händer när man klickar på baskartan, inte på en feature.
+			//console.log(e.originalEvent.target.id)
+			clearActiveSelectedParking()
+			var popupContent = ''
+			popupContent += '<form style="width:200px" js_action="https://script.google.com/macros/s/AKfycbyHBSts9-u_ixD-ZfidpQpABO_173B9OaklBGCT/exec" id="gform">'
+			popupContent += '<input type="text" name="ParkName" class="form-control" value="" placeholder="New park name" pattern=".{2,}" required title="2 characters minimum">'
+			popupContent += '<input type="text" name="LocationDescription" class="form-control" value="" placeholder="Location Description" pattern=".{10,}" required title="10 characters minimum">'
+			popupContent += '<input type="hidden" value="aNewPark" name="WhichPark">'
+			popupContent += '<input type="hidden" value="' + e.latlng.lng + '" name="ParkLocationLon">'
+			popupContent += '<input type="hidden" value="' + e.latlng.lat + '" name="ParkLocationLat">'
+			popupContent += '<button type="button" id="submit_button" class="btn btn-primary" onClick="jsSubmitForm(this.form)">Submit</button>'
+			popupContent += '</form>'
+			if (typeof(parkMarkerToBeAdded) === 'undefined') {
+				parkMarkerToBeAdded = new L.circle(e.latlng, {
+					draggable: true,
+					radius: 0,
+					weight: 0
+				});
+				parkMarkerToBeAdded.bindPopup(popupContent, {
+					maxWidth: "auto"
+				})
+				parkMarkerToBeAdded.addTo(map);
+				/*$("#gform").submit(function() {
+					jsSubmitForm($("#gform"))
+					return false;
+				});*/
+			} else {
+				parkMarkerToBeAdded.setLatLng(e.latlng);
+			}
+			parkMarkerToBeAdded.bindPopup(popupContent)
+
+			parkMarkerToBeAdded.openPopup()
+		}
+	}
+})
 
 function updateCurrentLocationMarker(e) {
 	//L.marker(e.latlng).addTo(map)
@@ -80,10 +95,10 @@ function updateCurrentLocationMarker(e) {
 }
 
 map.on('locationfound', updateCurrentLocationMarker);
-/*map.locate({
+map.locate({
 	setView: true,
 	zoom: 12,
-});*/
+});
 
 //Add layers to top right menu
 //L.control.layers(baseMaps).addTo(map)
@@ -125,30 +140,6 @@ function CSVToArray(strData, strDelimiter) {
 		arrData[arrData.length - 1].push(strMatchedValue);
 	}
 	return (arrData);
-}
-var colors = {
-	'red99': '#e6194B',
-	'green99': '#3cb44b',
-	'yellow100': '#ffe119',
-	'blue100': '#4363d8',
-	'orange9999': '#f58231',
-	'purple95': '#911eb4',
-	'cyan99': '#42d4f4',
-	'magenta99': '#f032e6',
-	'lime95': '#bfef45',
-	'pink9999': '#fabebe',
-	'teal99': '#469990',
-	'lavender9999': '#e6beff',
-	'brown99': '#9A6324',
-	'beige99': '#fffac8',
-	'maroon9999': '#800000',
-	'mint99': '#aaffc3',
-	'olive95': '#808000',
-	'apricot95': '#ffd8b1',
-	'navy9999': '#000075',
-	'grey100': '#a9a9a9',
-	'white100': '#ffffff',
-	'black100': '#000000',
 }
 
 
